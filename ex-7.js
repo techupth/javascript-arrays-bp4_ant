90c6921b69dc492c681a50bcf02e4e05521518d8
konthron gongmanee <konthron.ant@gmail.com>
2024-06-14 23:44:08 +0700

    feat:arrays assignment finish

--- ex-7.js
+++ ex-7.js
@@ -42,7 +42,14 @@ let orders = [
 ];
 
 // Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = orders.length;
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa"
+let productQuantityOfJoannet = orders[3].productQuantity;
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity;
+orders.shift()
+console.log(orders)
+console.log(isArray)
+console.log(creditCardTypeOfBlindermann)
+console.log(productQuantityOfJoannet)
+console.log(totalPurchaseOfDary)
